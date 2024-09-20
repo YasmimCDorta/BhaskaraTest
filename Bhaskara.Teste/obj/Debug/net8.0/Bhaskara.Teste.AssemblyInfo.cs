87a0d893077545b4e2619ee557a8f5bf237db313
“Yasmim <yasmin.dorta@aluno.senai.br>
2024-09-20 08:14:53 -0300

    Segundo commit

--- Bhaskara.Teste/obj/Debug/net8.0/Bhaskara.Teste.AssemblyInfo.cs
+++ Bhaskara.Teste/obj/Debug/net8.0/Bhaskara.Teste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bhaskara.Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93f3e16323c88217c94cc85589c1fc80d91b248")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bhaskara.Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bhaskara.Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
